5f45c5dd627f9dbb42756465ab5f05bb3688d6d6
Felippe Scheidt <felippescheidt@gmail.com>
2021-03-05 09:45:01 -0300

    initial commit

---
+++ src/main/java/br/com/cineclube/model/Filme.java
@@ -0,0 +1,11 @@
+package br.com.cineclube.model;
+
+public class Filme {
+	
+	private long filmeId;
+	private String nome;
+	private Integer ano; // 2016
+	private String categoria; // drama, action, ...
+	private Float nota; // 0..10
+
+}
