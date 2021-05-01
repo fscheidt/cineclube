36c9263c4387174ddef0cc62a4b5577c7d443811
Felippe Scheidt <felippescheidt@gmail.com>
2021-05-01 12:02:07 -0300

    api rest pessoa

--- src/main/java/br/com/cineclube/controller/ApiController.java
+++ src/main/java/br/com/cineclube/controller/ApiController.java
@@ -1,12 +1,22 @@
 package br.com.cineclube.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import br.com.cineclube.dao.PessoaRepository;
 import br.com.cineclube.model.Pessoa;
 
@@ -25,4 +35,53 @@ public class ApiController {
 		}
 		return dao.findByNomeIgnoreCaseContaining(query);
 	}
+	/* 
+	 * API PESSOA 
+	 * 
+	 * */
+	// READ
+	// `http :8080/api/pessoa/1`
+	// `http GET :8080/api/pessoa/1`
+	// optional trata NPE
+	@GetMapping("/pessoa/{id}")
+	Optional<Pessoa> getPessoa(@PathVariable Long id) { 
+		return dao.findById(id);
+	}
+	// Get ALL
+	// `http :8080/api/pessoas`
+	@GetMapping(value = "/pessoas") 
+	Iterable<Pessoa> gePessoas() { 
+		return dao.findAll();
+	}
+	// CREATE
+	@PostMapping("/pessoa")
+	Pessoa postPessoa(@RequestBody Pessoa pessoa) {
+		dao.save(pessoa);
+		return pessoa;
+	}
+	
+	// UPDATE
+	/**
+	 * `http PUT :8080/api/pessoa/4 < pessoa_kate.json`
+	 * `http PUT :8080/api/pessoa/111 < pessoa_maria.json`
+	 */
+	@PutMapping("/pessoa/{id}")
+	ResponseEntity<Pessoa> putPessoa(@PathVariable Long id, @RequestBody Pessoa pessoa) {
+		Pessoa p = dao.save(pessoa);
+		if(p!=null)
+			return new ResponseEntity<>(pessoa, HttpStatus.CREATED);
+		
+		return new ResponseEntity<>(pessoa, HttpStatus.OK);
+	}
+	
+	// DELETE
+	/**
+    * DELETE == DELETE
+    * `http DELETE :8080/api/pessoa/7`
+    */
+   @DeleteMapping("/pessoa/{id}")
+   void deletePessoa(@PathVariable Long id) {
+       dao.removerPessoa(id);
+   }
+	
 }
